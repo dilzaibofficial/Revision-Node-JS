05aad6b537aea8a2e8b2e79ccfd90e4910465343
DIL ZAIB <dilzaib5848@gmail.com>
2022-09-08 13:50:02 -0700

    first

---
+++ data.js
@@ -0,0 +1,6 @@
+const data=[
+    {name:'Dil',email:'dilzaib5848@gmail.com'},
+    {name:'Dilzaib',email:'dilzaib5848@gmail.com'},
+    {name:'Dilzaib Palh',email:'dilzaib5848@gmail.com'}
+]
+module.exports=data;
